e5d90f542bb698572f95e1b95afcdcaa89d01bce
saravanan4929 <57858663+saravanan4929@users.noreply.github.com>
2019-11-17 19:04:28 +0530

    Create ErrorCode.java

---
+++ ErrorCode.java
@@ -0,0 +1,6 @@
+public class ErrorCode {
+    public static final String TASK_ALREADY_ASSIGNED_EXCEPTION = "10000";
+    public static final String TASK_ASSIGNMENT_CLAIM_EXCEPTION = "10001";
+    public static final String TASK_NOT_ASSIGNED_DUE_NO_ACCESS_EXCEPTION = "10002";
+    public static final String USER_NOT_IN_TEAM_EXCEPTION = "10003";
+    }
